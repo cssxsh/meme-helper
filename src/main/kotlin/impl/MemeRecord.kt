89bf9030f104fc51c4576610251dbe9b0d160910
cssxsh <cssxsh@gmail.com>
2022-07-03 12:32:37 +0800

    fix: face record

--- src/main/kotlin/impl/MemeRecord.kt
+++ src/main/kotlin/impl/MemeRecord.kt
@@ -3,6 +3,7 @@ package xyz.cssxsh.mirai.meme.impl
 import net.mamoe.mirai.console.permission.*
 import net.mamoe.mirai.event.events.*
 import net.mamoe.mirai.message.data.*
+import net.mamoe.mirai.message.data.Image.Key.isUploaded
 import xyz.cssxsh.mirai.hibernate.*
 import xyz.cssxsh.mirai.hibernate.entry.*
 import xyz.cssxsh.mirai.meme.service.*
@@ -10,7 +11,7 @@ import java.io.File
 import java.util.*
 
 public class MemeRecord : MemeService {
-    override val name: String = "face record"
+    override val name: String = "Face Record"
     override val id: String = "record"
     override val description: String = "从群聊记录里获得表情包"
     override var loaded: Boolean = true
@@ -40,6 +41,12 @@ public class MemeRecord : MemeService {
     override fun disable() {}
 
     override suspend fun MessageEvent.replier(match: MatchResult): MessageContent {
+        var i = 3
+        while (i-- > 0) {
+            val message = FaceRecord.random().toMessageContent()
+            if (message is Image && message.isUploaded(bot).not()) continue
+            return message
+        }
         return FaceRecord.random().toMessageContent()
     }
 }
