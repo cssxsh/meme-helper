d9510bb87db00641ccc35ebfbe7ebde8100aa969
cssxsh <cssxsh@gmail.com>
2022-07-05 17:50:15 +0800

    build: 1.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "xyz.cssxsh"
-version = "1.1.0"
+version = "1.1.1"
 
 repositories {
     mavenLocal()
--- src/main/kotlin/MemeHelperPlugin.kt
+++ src/main/kotlin/MemeHelperPlugin.kt
@@ -11,7 +11,7 @@ public object MemeHelperPlugin : KotlinPlugin(
     JvmPluginDescription(
         id = "xyz.cssxsh.mirai.plugin.meme-helper",
         name = "meme-helper",
-        version = "1.1.0",
+        version = "1.1.1",
     ) {
         author("cssxsh")
         dependsOn("xyz.cssxsh.mirai.plugin.mirai-skia-plugin", ">= 1.1.0", false)
