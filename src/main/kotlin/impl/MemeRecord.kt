7bede40152c22c63eaaf8eef1cf8340118b9558d
cssxsh <cssxsh@gmail.com>
2022-07-20 13:18:10 +0800

    fix: record

--- src/main/kotlin/impl/MemeRecord.kt
+++ src/main/kotlin/impl/MemeRecord.kt
@@ -1,11 +1,18 @@
 package xyz.cssxsh.mirai.meme.impl
 
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.utils.io.jvm.javaio.*
 import net.mamoe.mirai.console.permission.*
 import net.mamoe.mirai.event.events.*
 import net.mamoe.mirai.message.data.*
 import net.mamoe.mirai.message.data.Image.Key.isUploaded
+import net.mamoe.mirai.utils.ExternalResource.Companion.uploadAsImage
+import net.mamoe.mirai.utils.info
 import xyz.cssxsh.mirai.hibernate.*
 import xyz.cssxsh.mirai.hibernate.entry.*
+import xyz.cssxsh.mirai.meme.*
 import xyz.cssxsh.mirai.meme.service.*
 import java.io.File
 import java.util.*
@@ -15,12 +22,32 @@ public class MemeRecord : MemeService {
     override val id: String = "record"
     override val description: String = "从群聊记录里获得表情包"
     override var loaded: Boolean = true
-    override var regex: Regex = """^#群友表情""".toRegex()
+    override var regex: Regex = """^#群友表情\s(.*)|#(.*)#""".toRegex()
         private set
     override val properties: Properties = Properties().apply { put("regex", regex.pattern) }
     override lateinit var permission: Permission
+    private var folder: File = File(System.getProperty("user.dir") ?: ".").resolve(".record")
+
+    private suspend fun FaceRecord.download(): File {
+        return folder.listFiles { file -> file.name.startsWith(md5) }?.firstOrNull()
+            ?: http.prepareGet(url).execute { response ->
+                val type = response.contentType()?.contentSubtype ?: "mirai"
+
+                val file = folder.resolve("$md5.$type")
+
+                logger.info { "文件 ${file.name} 开始下载" }
+                file.outputStream().use { output ->
+                    val channel = response.bodyAsChannel()
+
+                    while (!channel.isClosedForRead) channel.copyTo(output)
+                }
+
+                file
+            }
+    }
 
     override fun load(folder: File) {
+        this.folder = folder
         when (val re = properties["regex"]) {
             is String -> regex = re.toRegex()
             is Regex -> regex = re
@@ -41,12 +68,16 @@ public class MemeRecord : MemeService {
     override fun disable() {}
 
     override suspend fun MessageEvent.replier(match: MatchResult): MessageContent {
-        var i = 3
-        while (i-- > 0) {
-            val message = FaceRecord.random().toMessageContent()
-            if (message is Image && message.isUploaded(bot).not()) continue
-            return message
+        val tag = match.groupValues.last { it.isNotEmpty() }
+        val record = if (tag.isEmpty()) {
+            FaceRecord.random()
+        } else {
+            FaceRecord.match(tag = tag).randomOrNull() ?: return "查找失败 <${tag}>".toPlainText()
         }
-        return FaceRecord.random().toMessageContent()
+        val message = record.toMessageContent()
+        if (message is Image && message.isUploaded(bot).not()) {
+            return record.download().uploadAsImage(subject)
+        }
+        return message
     }
 }
