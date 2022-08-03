6cb52b347177adbcd7eeec205d382105cc880a3e
cssxsh <cssxsh@gmail.com>
2022-08-03 18:38:32 +0800

    update: kotlin 1.7.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    kotlin("jvm") version "1.6.21"
-    kotlin("plugin.serialization") version "1.6.21"
+    kotlin("jvm") version "1.7.10"
+    kotlin("plugin.serialization") version "1.7.10"
 
-    id("net.mamoe.mirai-console") version "2.12.0"
-    id("net.mamoe.maven-central-publish") version "0.7.1"
+    id("net.mamoe.mirai-console") version "2.12.1"
+    id("me.him188.maven-central-publish") version "1.0.0-dev-3"
 }
 
 group = "xyz.cssxsh"
@@ -19,17 +19,19 @@ mavenCentralPublish {
     useCentralS01()
     singleDevGithubProject("cssxsh", "meme-helper")
     licenseFromGitHubProject("AGPL-3.0", "master")
+    workingDir = System.getenv("PUBLICATION_TEMP")?.let { file(it).resolve(projectName) }
+        ?: project.buildDir.resolve("publishing-tmp")
     publication {
         artifact(tasks.getByName("buildPlugin"))
     }
 }
 
 dependencies {
-    compileOnly("xyz.cssxsh.mirai:mirai-hibernate-plugin:2.4.0")
-    compileOnly("xyz.cssxsh.mirai:mirai-skia-plugin:1.1.6")
+    compileOnly("xyz.cssxsh.mirai:mirai-hibernate-plugin:2.4.3")
+    compileOnly("xyz.cssxsh.mirai:mirai-skia-plugin:1.1.7")
     compileOnly("xyz.cssxsh:weibo-helper:1.5.3")
     compileOnly("xyz.cssxsh:bilibili-helper:1.6.1")
-    compileOnly("net.mamoe:mirai-core-utils:2.12.0")
+    compileOnly("net.mamoe:mirai-core-utils:2.12.1")
     implementation("io.ktor:ktor-client-okhttp:2.0.3") {
         exclude(group = "org.jetbrains.kotlin")
         exclude(group = "org.jetbrains.kotlinx")
@@ -46,9 +48,9 @@ dependencies {
         exclude(group = "org.slf4j")
     }
     // Test
-    testImplementation(kotlin("test", "1.6.21"))
-    testImplementation("xyz.cssxsh.mirai:mirai-skia-plugin:1.1.6")
-    testImplementation("net.mamoe:mirai-core-utils:2.12.0")
+    testImplementation(kotlin("test"))
+    testImplementation("xyz.cssxsh.mirai:mirai-skia-plugin:1.1.7")
+    testImplementation("net.mamoe:mirai-core-utils:2.12.1")
 }
 
 mirai {
