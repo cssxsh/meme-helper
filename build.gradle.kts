4de601b923dc166c53cedc3c57dca697497659e2
cssxsh <cssxsh@gmail.com>
2022-08-07 21:17:21 +0800

    change: publish info

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 mavenCentralPublish {
     useCentralS01()
     singleDevGithubProject("cssxsh", "meme-helper")
-    licenseFromGitHubProject("AGPL-3.0", "master")
+    licenseFromGitHubProject("AGPL-3.0")
     workingDir = System.getenv("PUBLICATION_TEMP")?.let { file(it).resolve(projectName) }
         ?: project.buildDir.resolve("publishing-tmp")
     publication {
